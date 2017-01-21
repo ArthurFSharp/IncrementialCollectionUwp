3bb83eafff7c8aa34839c4eead108fc9bec0385b
arthu <arthu@DESKTOP-AIFGUTL>
2017-01-21 12:23:19 +0000

    Ajoutez des fichiers projet.

---
+++ IncrementialCollectionSample/MainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementialCollectionSample
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private IncrementalLoadingCollection<PersonSource, Person> _collection;
+        public IncrementalLoadingCollection<PersonSource, Person> Collection
+        {
+            get { return _collection; }
+            set
+            {
+                _collection = value;
+                RaisePropertyChanged();
+            }
+        }
+        public MainPageViewModel()
+        {
+            Collection = new IncrementalLoadingCollection<PersonSource, Person>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ IncrementialCollectionSample/PersonSource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementialCollectionSample
+{
+    public class PersonSource : IIncrementalSource<Person>
+    {
+        private List<Person> persons;
+
+        public PersonSource()
+        {
+            persons = new List<Person>();
+
+            for (int i = 0; i < 1024; i++)
+            {
+                var p = new Person { Name = "Person " + i };
+                persons.Add(p);
+            }
+        }
+
+        public async Task<IEnumerable<Person>> GetPagedItems(int pageIndex, int pageSize)
+        {
+            return await Task.Run<IEnumerable<Person>>(() =>
+            {
+                var result = (from p in persons
+                              select p).Skip(pageIndex * pageSize).Take(pageSize);
+
+                return result;
+            });
+        }
+    }
+}
